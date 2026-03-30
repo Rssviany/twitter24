314b1926223c35fe6bfadf215352aded15ee690c
Rss <rssvinaykumar3801@gmail.com>
2026-03-30 20:50:01 +0530

    Connected frontend to deployed backend

--- Frontend/src/api/AxiosInstance.js
+++ Frontend/src/api/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance=axios.create({
-    baseURL:'http://localhost:6868/',
+    baseURL:'https://twitter24-backend.onrender.com',
     withCredentials:true,
     headers:{
         'Content-Type':'application/json'
