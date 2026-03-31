e2c41c46d93902947e3c84262df0f4e7035b271f
Rss <rssvinaykumar3801@gmail.com>
2026-03-31 10:20:10 +0530

    Fix cookies for production auth

--- Backend/controllers/SignIn&Up.js
+++ Backend/controllers/SignIn&Up.js
@@ -48,8 +48,8 @@ export const login = async (req, res) => {
         )
         res.cookie('token', token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax",
+            secure: true,
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         await res.json({ id: user._id, name: user.name, email: user.email })
